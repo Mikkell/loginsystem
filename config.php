aa908cb0cb8054c8505cd8bccf5d00bece68b9b4
Mikkell <mikkel_lindemark@hotmail.com>
2014-05-25 12:59:41 +0200

    tilføjelser til loginsystem

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/* Database login detaljer*/  
+define("HOST", "localhost");     // Navnet på host.
+define("USER", "input_bruger");    // Databasebrugeren. 
+define("PASSWORD", "7aC4VwZa7z6YrTGr");    // Koden til databasen. 
+define("DATABASE", "sikkerhed_login");    // Databasenavnet.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+
+define("SECURE", FALSE); //under produktionen.
+
+?>
---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+include_once 'config.php';
+
+function sec_session_start() {
+	$session_name = 'sec_session_id'; //opret session id
+	$secure = SECURE;
+	// Dette 
+}
