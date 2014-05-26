b9cc244b67239d07c511e2809db99958297caa39
Mikkell <mikkel_lindemark@hotmail.com>
2014-05-26 14:42:42 +0200

    rettelser

--- register_success.php
+++ register_success.php
@@ -2,11 +2,11 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Secure Login: Registration Success</title>
-        <link rel="stylesheet" href="styles/main.css" />
+        <title>Loginsystem</title>
+        <link rel="stylesheet" href="css/style.css" />
     </head>
     <body>
-        <h1>Registration successful!</h1>
-        <p>You can now go back to the <a href="index.php">login page</a> and log in</p>
+        <h1>Succesful registrering</h1>
+        <p>Du kan gå tilbage til <a href="index.php">loginsiden</a> og logge ind.</p>
     </body>
 </html>
