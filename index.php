6aa13aff8c53d6026207b5dbf5ab66021ba8b959
Mikkell <mikkel_lindemark@hotmail.com>
2014-05-26 15:14:28 +0200

    forside

--- index.php
+++ index.php
@@ -9,24 +9,31 @@ if (login_check($mysqli) == true) {
 } else {
     $logged = 'out';
 }
-?>
-<?php include("header.php"); ?>
-<?php
+
+include("header.php");
+
 if (isset($_GET['error'])) {
     echo '<p class="error">Error Logging In!</p>';
 }
 ?> 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Loginsystem</title>
+</head>
+<body>
 <form action="includes/process_login.php" method="post" name="login_form">                      
     Email: <input type="text" name="email" />
-    Password: <input type="password" 
-                     name="password" 
-                     id="password"/>
+    Password: <input type="kode" 
+                     name="kode" 
+                     id="kode"/>
     <input type="button" 
            value="Login" 
-           onclick="formhash(this.form, this.form.password);" /> 
+           onclick="formhash(this.form, this.form.kode);" /> 
 </form>
-<p>If you don't have a login, please <a href="register.php">register</a></p>
-<p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
-<p>You are currently logged <?php echo $logged ?>.</p>
-
+<p>Hvis du ikke har en bruger,  <a href="register.php">register dig</a></p>
+<p>Hvis du er færdig, <a href="includes/logout.php">log ud</a>.</p>
+<p>Du er nu <?php echo $logged ?>.</p>
+</body>
+</html>
 <?php include("footer.php"); ?>
